50867f303bcc24b4160be301f09da29c27d129b5
salvomel <salv.mel@gmail.com>
2021-11-18 17:01:15 +0100

    milestone-2

---
+++ milestone-2/js/script.js
@@ -0,0 +1,56 @@
+// ARRAYS
+const items = [
+    'img/01.jpg',
+    'img/02.jpg',
+    'img/03.jpg',
+    'img/04.jpg',
+    'img/05.jpg'
+];
+
+const title = [
+    'Svezia',
+    'Svizzera',
+    'Gran Bretagna',
+    'Germania',
+    'Paradise'
+]
+
+const text = [
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+    'Lorem ipsum',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam.',
+]
+
+// INSERISCO IMMAGINI
+
+const bigImageContainer = document.querySelector('.big-image-container');
+// const thumbs = document.querySelector('.thumbs');
+
+// POPOLO CONTAINER PER OGNI ELEMENTO DELL'ARRAY
+for(let i=0; i<items.length; i++) {
+    const thisPath = items[i];
+    const thisTitle = title[i];
+    const thisText = text[i];
+
+    const newImage = `
+    <div class="single-image>
+        <img src="${thisPath}" alt="Img ${i}">
+
+        <div class="single-image-text">
+            <h3>${thisTitle}</h3>
+            <p>${thisText}</p>
+        </div>    
+    </div>    
+    `;
+
+    bigImageContainer.innerHTML += newImage;
+}
+
+// CLASSE ACTIVE
+let activeImage = 0;
+const allImages = document.getElementsByClassName('single-image');
+allImages[activeImage].classList.add('active');
+
+
